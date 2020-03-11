fd91c0ef49a82109c1d6b09512136a9e4073aa1e
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-03-11 21:52:23 +0900

    Style MemoDetailScreen

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default class CircleButton extends Component {
   render() {
+    const { style, color } = this.props;
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
     return (
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonTitle}>{this.props.children}</Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+          {this.props.children}
+        </Text>
       </View>
     );
   }
@@ -18,7 +28,6 @@ const styles = StyleSheet.create({
     right: 32,
     width: 48,
     height: 48,
-    backgroundColor: '#E31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
@@ -29,7 +38,6 @@ const styles = StyleSheet.create({
   },
   circleButtonTitle: {
     fontSize: 32,
-    lineHeight: 32,
-    color: '#fff'
+    lineHeight: 32
   }
 });
---
+++ src/screens/MemoDetailScreen.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import moduleName from '../elements/CircleButton';
+import CircleButton from '../elements/CircleButton';
+
+export default class MemoDetailScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.memoHeader}>
+          <View>
+            <Text style={styles.memoHeaderTitle}>講座のアイデア</Text>
+            <Text style={styles.memoHeaderDate}>2020/03/11</Text>
+          </View>
+        </View>
+        <View style={styles.memoContent}>
+          <Text>講座のアイデアです。</Text>
+        </View>
+        <CircleButton color="white" style={styles.editButton}>
+          +
+        </CircleButton>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  },
+  memoHeader: {
+    height: 100,
+    backgroundColor: '#17313C',
+    justifyContent: 'center',
+    padding: 10
+  },
+  memoHeaderTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 4
+  },
+  memoHeaderDate: {
+    color: 'white',
+    fontSize: 12
+  },
+  memoContent: {
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 20,
+    backgroundColor: '#fff',
+    flex: 1
+  },
+  editButton: {
+    top: 78
+  }
+});
